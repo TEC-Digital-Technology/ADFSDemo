30e0cb8e104638a172d0087f56f502aaf053419e
Antony <antony@tecyt.com>
2019-03-19 20:58:00 +0800

    1. 加入 TEC Internal Utils 基本設定

---
+++ TEC.AdfsDemo.Web/TEC.AdfsDemo.Web/Enums/DemoWebSettingEnum.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TEC.AdfsDemo.Web.Enums
+{
+    /// <summary>
+    /// ADFS Demo 網站的設定檔列舉
+    /// </summary>
+    public enum DemoWebSettingEnum
+    {
+        /// <summary>
+        /// 使用 AES-256 加解密時需要的 Key，屬於<see cref="String"/>
+        /// </summary>
+        AES256Key,
+        /// <summary>
+        /// 使用 AES-256 加解密時需要的 IV，屬於<see cref="String"/>
+        /// </summary>
+        AES256IV,
+        /// <summary>
+        /// 用於身分識別的應用程式金鑰，屬於 <see cref="String"/>
+        /// </summary>
+        AppKey,
+        /// <summary>
+        /// 認證提供者位址，屬於 <see cref="String"/>
+        /// </summary>
+        Authority,
+        /// <summary>
+        /// 信賴識別碼，屬於 <see cref="String"/>
+        /// </summary>
+        AllowedAudience,
+        /// <summary>
+        /// 簽署憑證路徑，屬於 <see cref="Uri"/>
+        /// </summary>
+        SigningCertPath,
+        /// <summary>
+        /// 客戶端識別碼，屬於 <see cref="String"/>
+        /// </summary>
+        ClientId,
+        /// <summary>
+        /// 取得資源識別碼，屬於 <see cref="String"/>
+        /// </summary>
+        GraphResourceId,
+        /// <summary>
+        /// 平台名稱，用於介接內部系統使用(TEC Internal Utils)，屬於 <see cref="String"/>
+        /// </summary>
+        PlatformName,
+        /// <summary>
+        /// 帳號資料伺服器位址，用於介接內部系統使用(TEC Internal Utils)，屬於 <see cref="Uri"/>
+        /// </summary>
+        AccountServiceApiBaseLocation
+    }
+}
---
+++ TEC.AdfsDemo.Web/TEC.AdfsDemo.Web/Settings/SettingCollectionFactoryInternal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using TEC.AdfsDemo.Web.Enums;
+using TEC.AdfsDemo.Web.Settings.Collections;
+using TEC.Core.Settings.Providers;
+
+namespace TEC.AdfsDemo.Web.Settings
+{
+    /// <summary>
+    /// 設定檔集合工廠
+    /// </summary>
+    internal static class SettingCollectionFactoryInternal
+    {
+        private static readonly object sync_obj = new object();
+        /// <summary>
+        /// 取得應用程式的設定檔集合
+        /// </summary>
+        public static DemoWebSettingCollection DemoWebSettingCollection
+        {
+            get
+            {
+                ApplicationSettingProvider<DemoWebSettingCollection, DemoWebSettingEnum, object, string> provider =
+                    new ApplicationSettingProvider<DemoWebSettingCollection, DemoWebSettingEnum, object, string>();
+
+                if (provider.load() == null)
+                {
+                    lock (SettingCollectionFactoryInternal.sync_obj)
+                    {
+                        if (provider.load() == null)
+                        {
+                            DemoWebSettingCollection demoWebSettingCollection = new DemoWebSettingCollection
+                            {
+                                [DemoWebSettingEnum.AccountServiceApiBaseLocation] = new Uri(ConfigurationManager.AppSettings["AccountServiceApiBaseLocation"]),
+                                [DemoWebSettingEnum.AllowedAudience] = ConfigurationManager.AppSettings["ida:AllowedAudience"],
+                                [DemoWebSettingEnum.AppKey] = ConfigurationManager.AppSettings["ida:AppKey"],
+                                [DemoWebSettingEnum.Authority] = ConfigurationManager.AppSettings["ida:Authority"],
+                                [DemoWebSettingEnum.ClientId] = ConfigurationManager.AppSettings["ida:ClientId"],
+                                [DemoWebSettingEnum.GraphResourceId] = ConfigurationManager.AppSettings["ida:GraphResourceId"],
+                                [DemoWebSettingEnum.SigningCertPath] = new Uri(ConfigurationManager.AppSettings["ida:SingingCertPath"]),
+                                [DemoWebSettingEnum.PlatformName] = ConfigurationManager.AppSettings["PlatformName"],
+                                [DemoWebSettingEnum.AES256Key] = ConfigurationManager.AppSettings["AES256Key"],
+                                [DemoWebSettingEnum.AES256IV] = ConfigurationManager.AppSettings["AES256IV"],
+                            };
+
+                            provider.save(demoWebSettingCollection);
+                        }
+                    }
+                }
+
+                return provider.load();
+            }
+        }
+    }
+}
