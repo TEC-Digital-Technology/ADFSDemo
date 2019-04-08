a91814a684883ed53e9b7c838133274ebea7e1d7
Yumin <Yumin@tecyt.com>
2019-04-08 12:16:14 +0800

    修成 Controller 繼承

--- TEC.AdfsDemo.Web/TEC.AdfsDemo.WebApi/Controllers/DefaultController.cs
+++ TEC.AdfsDemo.Web/TEC.AdfsDemo.WebApi/Controllers/DefaultController.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TEC.Internal.Utils.Core.Info;
 using TEC.Internal.Utils.WebApi.Attributes;
 
 namespace TEC.AdfsDemo.WebApi.Controllers
 {
-    public class DefaultController : ApiController
+    public class DefaultController : TEC.Internal.Utils.WebApi.Controller.ApiControllerBase
     {
         [OAuthAuthorize]
-        public void index() {
-
+        public void index()
+        {
+            //取得帳號資料
+            AccountCliamsInfo accountCliamsInfo = base.AccountCliamsInfo;
         }
     }
 }
